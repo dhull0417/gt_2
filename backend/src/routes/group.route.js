6b023380c85711ebb72a70706065e1f8b09a9a22
Dallin Hull <dallin.hull@outlook.com>
2025-09-04 16:06:19 -0600

    Only allowing the create group route

--- backend/src/routes/group.route.js
+++ backend/src/routes/group.route.js
@@ -1,9 +1,6 @@
 import express from "express";
 import {
   createGroup,
-  getGroupProfile,
-  joinGroup,
-  leaveGroup,
 } from "../controllers/group.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
@@ -11,8 +8,8 @@ const router = express.Router();
 
 // protected routes
 router.post("/create", protectRoute, createGroup);
-router.get("/:groupId", protectRoute, getGroupProfile);
-router.post("/join/:groupId", protectRoute, joinGroup);
-router.post("/leave/:groupId", protectRoute, leaveGroup);
+//router.get("/:groupId", protectRoute, getGroupProfile);
+//router.post("/join/:groupId", protectRoute, joinGroup);
+//router.post("/leave/:groupId", protectRoute, leaveGroup);
 
 export default router;
