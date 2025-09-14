6236aff8a037646b11e76d9f4e6d01a7920e940a
Dallin Hull <dallin.hull@outlook.com>
2025-09-13 18:01:22 -0600

    get rid of update group details

--- backend/src/routes/group.route.js
+++ backend/src/routes/group.route.js
@@ -5,9 +5,8 @@ import {
   getGroupDetails,
   addMember,
   deleteGroup,
-  updateGroupDetails,
-  removeMember,
   leaveGroup,
+  removeMember,
 } from "../controllers/group.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
@@ -18,11 +17,9 @@ router.post("/create", protectRoute, createGroup);
 router.get("/:groupId", protectRoute, getGroupDetails);
 router.post("/:groupId/add-member", protectRoute, addMember);
 router.delete("/:groupId", protectRoute, deleteGroup);
-router.put("/:groupId/details", protectRoute, updateGroupDetails);
-
-// 2. Add the two new routes for member management
 router.post("/:groupId/leave", protectRoute, leaveGroup);
 router.post("/:groupId/remove-member", protectRoute, removeMember);
 
+// The route for 'updateGroupDetails' has been removed.
 
 export default router;
