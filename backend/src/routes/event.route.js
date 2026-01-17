45772472db922829a7ef935e97519875d272796d
Dallin Hull <dallin.hull@outlook.com>
2026-01-17 13:19:44 -0700

    Event Route update to include cancellations.

--- backend/src/routes/event.route.js
+++ backend/src/routes/event.route.js
@@ -1,11 +1,23 @@
 import express from "express";
-import { getEvents, handleRsvp, updateEvent, deleteEvent } from "../controllers/event.controller.js";
+import { 
+    getEvents, 
+    handleRsvp, 
+    updateEvent, 
+    deleteEvent, 
+    cancelEvent 
+} from "../controllers/event.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+// --- General ---
 router.get("/", protectRoute, getEvents);
+
+// --- RSVP Logic ---
 router.post("/:eventId/rsvp", protectRoute, handleRsvp);
+
+// --- Management ---
+router.patch("/:eventId/cancel", protectRoute, cancelEvent);
 router.put("/:eventId", protectRoute, updateEvent);
 router.delete("/:eventId", protectRoute, deleteEvent);
 
