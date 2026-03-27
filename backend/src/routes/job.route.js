55fe824923ea0918d35645a540e8c13b4ca168c7
Dallin Hull <dallin.hull@outlook.com>
2026-03-26 18:00:48 -0600

    Trying to solve expire meetings inaction.

--- backend/src/routes/job.route.js
+++ backend/src/routes/job.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { regenerateMeetups } from "../controllers/job.controller.js";
+import { regenerateMeetups, expirePastMeetups, cleanupExpiredMeetups } from "../controllers/job.controller.js";
 import { protectCron } from "../middleware/cron.middleware.js";
 
 const router = express.Router();
 
 router.post("/regenerate-meetups", protectCron, regenerateMeetups);
+router.post("/expire-meetups", protectCron, expirePastMeetups); // New route for expiring meetups
+router.post("/cleanup-meetups", protectCron, cleanupExpiredMeetups); // New route for cleaning up expired meetups
 
 export default router;
